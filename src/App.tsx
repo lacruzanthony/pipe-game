560c60048a0bddf1adee89732c9fe8ce99b0daa5
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-06-14 11:52:00 -0500

    removed Evolution img

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PipeGame from './containers/PipeGame'
+import PipeGame from './containers/PipeGame';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <header className="App-header">Pipe Game</header>
       <PipeGame />
     </div>
   );
